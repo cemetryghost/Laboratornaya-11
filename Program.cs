9f02436e0e1f6fb82aa3b40271cdd51885b07063
cemetryghost <96484941+cemetryghost@users.noreply.github.com>
2023-03-26 21:52:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp11
+{
+    internal class Program
+    {
+        enum DayOfWeekEnum { Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье };
+
+        delegate string GetNextDayOfWeekDelegate();
+        static void Main(string[] args)
+        {
+            int currentDayIndex = -1;
+
+            GetNextDayOfWeekDelegate getNextDay = () =>
+            {
+                currentDayIndex = (currentDayIndex + 1) % Enum.GetValues(typeof(DayOfWeekEnum)).Length;
+                return ((DayOfWeekEnum)currentDayIndex).ToString();
+            };
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"День недели - {getNextDay()}");
+            }
+        }
+    }
+}
